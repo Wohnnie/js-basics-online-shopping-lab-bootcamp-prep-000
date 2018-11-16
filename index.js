65cc0fb0282a52e848c4f0ce50d0769ecc8ec00b
Angie Quin <aquinones100@gmail.com>
2018-11-16 12:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,49 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+
+ var item_object = {
+   itemName: item,
+   itemPrice: price
+ };
+
+ cart.push(item_object);
+ console.log (`${item} has been added to your cart.`);
 }
+
+addToCart(apples)
+addToCart(bananas)
+addToCart(carrots)
+addToCart(daikon)
 
 function viewCart() {
-  // write your code here
+  var viewItems = [];
+
+  if(cart.length===0) {
+  console.log ("Your shopping cart is empty")
+
+} else {
+
+  for(i = 0; i < cart.length; i++) {
+    var item_object = cart[i];
+    var itemName = item_object.itemName;
+    var itemPrice = item_object.itemPrice;
+
+      if (cart.length===1) {
+      viewItems.push(`${itemName} at $${itemPrice}.`);
+
+    }
+  }
 }
 
+viewCart();
+
 function total() {
-  // write your code here
+  var priceOfItems = [];
+
+  for (i = 0; i < cart.length; i++)
+
 }
 
 function removeFromCart(item) {
